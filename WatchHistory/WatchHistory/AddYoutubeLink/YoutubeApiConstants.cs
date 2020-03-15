5d9dd9e264c69a75e0ede0b0feb15eabba34f688
Karsten Schramm <SchramKa@basfad.basf.net>
2020-03-15 07:38:31 +0100

    clean-up

--- WatchHistory/WatchHistory/AddYoutubeLink/YoutubeApiConstants.cs
+++ WatchHistory/WatchHistory/AddYoutubeLink/YoutubeApiConstants.cs
@@ -2,10 +2,6 @@
 {
     internal static class YoutubeApiConstants
     {
-        //internal const string ClientId = "<OAuth 2.0-Client-Id>";
-
-        //internal const string ClientSecret = "<OAuth 2.0-Client-Key>";
-
-        internal const string ApiKey = "AIzaSyBeAsvT6p7yTpu5E7fBlTurxncisu_maPs";
+        internal const string ApiKey = "<ApiKey>";
     }
 }
