28cc34191cc7e63a59da5517f2fd6caab9300191
DJDoena <github@doena-soft.de>
2023-03-04 21:15:28 +0100

    CleanUpHistory

--- CleanUpHistory/Properties/AssemblyInfo.cs
+++ CleanUpHistory/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Doena Soft.")]
 [assembly: AssemblyProduct("WatchHistory")]
-[assembly: AssemblyCopyright("Copyright © Doena Soft. 2017 - 2018")]
+[assembly: AssemblyCopyright("Copyright © Doena Soft. 2017 - 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.2.1")]
-[assembly: AssemblyFileVersion("1.0.2.1")]
+[assembly: AssemblyVersion("1.0.2.2")]
+[assembly: AssemblyFileVersion("1.0.2.2")]
