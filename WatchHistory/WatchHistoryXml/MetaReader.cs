41bba7c0041f68107462e16b960310de71b97fd9
DJ Doena <djdoena@outlook.com>
2026-04-17 14:39:01 +0200

    1.0.13

---
+++ WatchHistory/WatchHistoryXml/MetaReader.cs
@@ -0,0 +1,10 @@
+﻿using DoenaSoft.ToolBox.Generics;
+using DoenaSoft.WatchHistory.Data;
+
+namespace DoenaSoft.WatchHistory;
+
+public static class MetaReader
+{
+    public static Files ReadFiles(string fileName)
+        => XmlSerializer<Files>.Deserialize(fileName);
+}
